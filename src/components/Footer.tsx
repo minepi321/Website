0822ad6b7d2f8cff9ec99b9b376035a64182ecce
Timothy Babatunde Akanbi <knbtimothy@gmail.com>
2025-03-28 12:39:15 +0000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,30 +2,39 @@ import Image from "next/image"
 import Link from "next/link"
 
 const Footer = () => {
+    const BASE_URL = 'https://minepi.com'
     const Links = [
         {
-            text: 'Pi WHitepaper'
+            text: 'Pi Whitepaper',
+            url: `${BASE_URL}/white-paper/`
         },
         {
-            text: 'Support & FAQ'
+            text: 'Support & FAQ',
+            url: `${BASE_URL}/support/`
         },
         {
-            text: 'Community Code of Conduct'
+            text: 'Community Code of Conduct',
+            url: 'https://minepi.com/pioneer-code-of-conduct/'
         },
         {
-            text: 'Terms of Service'
+            text: 'Terms of Service',
+            url: 'https://socialchain.app/tos'
         },
         {
-            text: 'Privacy Policy'
+            text: 'Privacy Policy',
+            url: 'https://socialchain.app/privacy'
         },
         {
-            text: 'Developer Terms of Use'
+            text: 'Developer Terms of Use',
+            url: 'https://socialchain.app/developer_terms'
         },
         {
-            text: 'Pi Trademark'
+            text: 'Pi Trademark',
+            url: 'https://minepi.com/pi-trademark-guidelines/'
         },
         {
-            text: 'Safety Center'
+            text: 'Safety Center',
+            url: 'https://minepi.com/safety/'
         },
     ]
   return (
@@ -35,7 +44,7 @@ const Footer = () => {
                 {Links.map(link =>(
                     <li key={crypto.randomUUID()} className="hover:text-[#9244ac]">
                         <Link 
-                            href={''}
+                            href={link.url}
                             className=""
                         > 
                             {link.text} 
